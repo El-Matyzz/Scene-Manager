d0385dc50799abbffeb826aff87031fef2f36852
Mati <32552512+El-Matyzz@users.noreply.github.com>
2017-10-19 20:12:56 -0300

    2do Commit
    Ahora, en la ventana, se listan todas las escenas ubicadas en una carpeta Scenes dentro de Assets con sus respectivos nombres y funcionalidades.

--- Assets/Editor/Scene_Manager_Editor.cs
+++ Assets/Editor/Scene_Manager_Editor.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
+using System.IO;
 
 public class Scene_Manager_Editor : EditorWindow
 {
     public Object MainScene;
     public string SceneNameInspector;
+
+    public GUIStyle _style00;
+    public GUIStyle _style01;
+    public GUIStyle _style02;
 
     [MenuItem("Custom Windows/Scene Info")]
     static void CreateWindow()
@@ -23,20 +28,20 @@ public class Scene_Manager_Editor : EditorWindow
         #endregion
 
         #region _style00 (Large + Middle + Bold + (Font = 15) )
-        GUIStyle _style00 = EditorStyles.largeLabel;
+        _style00 = EditorStyles.largeLabel;
         _style00.alignment = TextAnchor.UpperCenter;
         _style00.fontStyle = FontStyle.Bold;
         _style00.fontSize = 15;
         #endregion
 
         #region _style01(Normal + Center)
-        GUIStyle _style01 = EditorStyles.label;
+        _style01 = EditorStyles.label;
         _style01.fontStyle = FontStyle.Normal;
         _style01.alignment = TextAnchor.MiddleCenter;
         #endregion
 
         #region _style02 (Large + Middle + Bold + (Font = 15) )
-        GUIStyle _style02 = EditorStyles.largeLabel;
+        _style02 = EditorStyles.largeLabel;
         _style02.alignment = TextAnchor.UpperCenter;
         _style02.fontStyle = FontStyle.Bold;
         _style02.fontSize = 15;
@@ -46,6 +51,7 @@ public class Scene_Manager_Editor : EditorWindow
         GUI.enabled = false;
         EditorGUILayout.TextArea("Scene Changer", _style02);
 
+        /*
         EditorGUILayout.Space();
         //Main Menu
         EditorGUILayout.BeginHorizontal();
@@ -65,7 +71,8 @@ public class Scene_Manager_Editor : EditorWindow
         EditorGUILayout.EndVertical();
         EditorGUILayout.EndHorizontal();
         EditorGUILayout.EndHorizontal();
-        
+        */
+        GetAllScenes();
 
         GUILayoutUtility.GetRect(20, 20);
 
@@ -75,6 +82,36 @@ public class Scene_Manager_Editor : EditorWindow
         maxSize = new Vector2(300, 500);
         #endregion
 
+    }
+
+    public void GetAllScenes()
+    {
+        DirectoryInfo dir = new DirectoryInfo("Assets/Scenes/");
+        FileInfo[] info = dir.GetFiles("*.unity");
+
+        for (int i = 0; i < info.Length; i++)
+        {
+            string sceneName = Path.GetFileNameWithoutExtension(info[i].Name);
+            
+            EditorGUILayout.Space();
+            EditorGUILayout.BeginHorizontal();
+            GUI.DrawTexture(GUILayoutUtility.GetRect(50, 50), (Texture2D)Resources.Load("Unity Icon"), ScaleMode.ScaleToFit);
+            EditorGUILayout.BeginVertical();
+            SceneNameInspector = sceneName;
+            EditorGUILayout.TextArea(SceneNameInspector, _style00);
+            GUI.enabled = true;
+            Rect buttonRect00 = EditorGUILayout.BeginHorizontal("Button");
+            if (GUI.Button(buttonRect00, GUIContent.none))
+            {
+                Main_Scene_Info.ChangeSceneTo_EditorMode(SceneNameInspector);
+            }
+            buttonRect00.height = 10;
+            buttonRect00.width = 50;
+            GUILayout.Label("Change Scene to " + SceneNameInspector, _style01);
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.EndHorizontal();
+        }
     }
 
 }
