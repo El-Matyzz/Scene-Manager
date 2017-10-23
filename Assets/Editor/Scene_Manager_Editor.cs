f9c6c62426130054525286bdc11c3ab2190d3beb
Manfreack <32555736+Manfreack@users.noreply.github.com>
2017-10-23 20:54:02 -0300

    3er Commit
    - Creador de Escenas. cumple con estas consignas:
       * No puedo generar escenas con el mismo nombre de otra escena.
       * No puedo generar escenas sin nombre.
    
    - Bugfixes:
       * Los nombres de las escenas del editor ya no pueden ser modificados momentaneamente si se los selecciona.

--- Assets/Editor/Scene_Manager_Editor.cs
+++ Assets/Editor/Scene_Manager_Editor.cs
@@ -8,12 +8,16 @@ using System.IO;
 
 public class Scene_Manager_Editor : EditorWindow
 {
-    public Object MainScene;
-    public string SceneNameInspector;
+    public Object mainScene;
+    public string sceneNameInspector;
+    public string newSceneName;
 
     public GUIStyle _style00;
     public GUIStyle _style01;
     public GUIStyle _style02;
+
+    public DirectoryInfo dir;
+    public FileInfo[] info;
 
     [MenuItem("Custom Windows/Scene Info")]
     static void CreateWindow()
@@ -23,9 +27,6 @@ public class Scene_Manager_Editor : EditorWindow
     void OnGUI()
     {
 
-        #region MainLayout - Do Not Touch
-        EditorGUILayout.BeginVertical();
-        #endregion
 
         #region _style00 (Large + Middle + Bold + (Font = 15) )
         _style00 = EditorStyles.largeLabel;
@@ -47,9 +48,17 @@ public class Scene_Manager_Editor : EditorWindow
         _style02.fontSize = 15;
         #endregion
 
+
+        #region Title - || Scene Manager ||
+
+        EditorGUILayout.BeginVertical();
         EditorGUILayout.Space();
         GUI.enabled = false;
         EditorGUILayout.TextArea("Scene Changer", _style02);
+        GUI.enabled = true;
+
+        #endregion
+
 
         /*
         EditorGUILayout.Space();
@@ -73,6 +82,7 @@ public class Scene_Manager_Editor : EditorWindow
         EditorGUILayout.EndHorizontal();
         */
         GetAllScenes();
+        CreateScene();
 
         GUILayoutUtility.GetRect(20, 20);
 
@@ -86,32 +96,96 @@ public class Scene_Manager_Editor : EditorWindow
 
     public void GetAllScenes()
     {
-        DirectoryInfo dir = new DirectoryInfo("Assets/Scenes/");
-        FileInfo[] info = dir.GetFiles("*.unity");
+        dir = new DirectoryInfo("Assets/Scenes/");
+        info = dir.GetFiles("*.unity");
 
         for (int i = 0; i < info.Length; i++)
         {
             string sceneName = Path.GetFileNameWithoutExtension(info[i].Name);
-            
+            GUI.enabled = false;
             EditorGUILayout.Space();
             EditorGUILayout.BeginHorizontal();
             GUI.DrawTexture(GUILayoutUtility.GetRect(50, 50), (Texture2D)Resources.Load("Unity Icon"), ScaleMode.ScaleToFit);
             EditorGUILayout.BeginVertical();
-            SceneNameInspector = sceneName;
-            EditorGUILayout.TextArea(SceneNameInspector, _style00);
+            sceneNameInspector = sceneName;
+            EditorGUILayout.TextArea(sceneNameInspector, _style00);
             GUI.enabled = true;
             Rect buttonRect00 = EditorGUILayout.BeginHorizontal("Button");
             if (GUI.Button(buttonRect00, GUIContent.none))
             {
-                Main_Scene_Info.ChangeSceneTo_EditorMode(SceneNameInspector);
+                Main_Scene_Info.ChangeSceneTo_EditorMode(sceneNameInspector);
             }
             buttonRect00.height = 10;
             buttonRect00.width = 50;
-            GUILayout.Label("Change Scene to " + SceneNameInspector, _style01);
+            GUILayout.Label("Change Scene to " + sceneNameInspector, _style01);
             EditorGUILayout.EndVertical();
             EditorGUILayout.EndHorizontal();
             EditorGUILayout.EndHorizontal();
         }
+    }
+
+    void CreateScene()
+    {
+
+        #region Title || Create Scene ||
+
+        string SceneNameInspector = "Create Scene";
+        GUILayoutUtility.GetRect(20, 50);
+        GUI.enabled = false;
+        EditorGUILayout.TextArea(SceneNameInspector, _style00);
+        GUI.enabled = true;
+        EditorGUILayout.Space();
+
+        #endregion
+
+        EditorGUILayout.BeginHorizontal();
+        GUI.DrawTexture(GUILayoutUtility.GetRect(50, 50), (Texture2D)Resources.Load("Unity Icon"), ScaleMode.ScaleToFit);
+        EditorGUILayout.BeginVertical();
+
+        newSceneName = EditorGUILayout.TextField("New scene name: ", newSceneName);
+        Rect buttonRect01 = EditorGUILayout.BeginHorizontal("Button");
+
+        GUI.enabled = true;
+        for (int i = 0; i < info.Length; i++)
+        {
+            if (newSceneName == "" || newSceneName == Path.GetFileNameWithoutExtension(info[i].Name))
+            {
+                GUI.enabled = false;
+            }
+        }
+
+        if (GUI.Button(buttonRect01, GUIContent.none))
+        {
+            EditorSceneManager.SaveScene(EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Single), "Assets/Scenes/" + newSceneName + ".unity");
+        }
+
+        buttonRect01.height = 10;
+        buttonRect01.width = 50;
+        GUI.enabled = true;
+        GUILayout.Label("Create new scene as: " + newSceneName, _style01);
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayoutUtility.GetRect(50, 50);
+
+        for (int i = 0; i < info.Length; i++)
+        {
+            if (newSceneName == Path.GetFileNameWithoutExtension(info[i].Name))
+            {
+                EditorGUILayout.HelpBox("Why are you using the same scene name again? ヽ(ﾟДﾟ)ﾉ", MessageType.Warning);
+            }
+        }
+
+        if (newSceneName == "")
+        {
+            EditorGUILayout.HelpBox("You should put something up here or it won't work (>_<)", MessageType.Warning);
+        }
+
+
+        EditorGUILayout.EndHorizontal();
+
     }
 
 }
